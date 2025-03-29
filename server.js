f7041971faeea49a5cfc46b74ec7e36444910b7e
Merzaad <merzaad@gmail.com>
2025-03-29 09:55:19 +0330

    single queries

--- server.js
+++ server.js
@@ -12,10 +12,16 @@ import {
 const strings = [
   { id: 1, string: "one" },
   { id: 2, string: "two" },
+  { id: 3, string: "three" },
+  { id: 4, string: "four" },
+  { id: 5, string: "five" },
 ];
 const numbers = [
   { id: 1, number: 1 },
   { id: 2, number: 2 },
+  { id: 3, number: 3 },
+  { id: 4, number: 4 },
+  { id: 5, number: 5 },
 ];
 
 const StringsType = new GraphQLObjectType({
@@ -24,6 +30,22 @@ const StringsType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLInt) },
     string: { type: GraphQLNonNull(GraphQLString) },
+    number: {
+      type: NumbersType,
+      resolve: (string) => numbers.find((number) => number.id === string.id),
+    },
+  }),
+});
+const NumbersType = new GraphQLObjectType({
+  name: "Numbers",
+  description: "Numbers GraphQLObjectType",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    number: { type: GraphQLNonNull(GraphQLInt) },
+    string: {
+      type: StringsType,
+      resolve: (number) => strings.find((string) => string.id === number.id),
+    },
   }),
 });
 const RootQueryType = new GraphQLObjectType({
@@ -34,6 +56,29 @@ const RootQueryType = new GraphQLObjectType({
       type: new GraphQLList(StringsType),
       description: "list of Strings",
       resolve: () => strings,
+    },
+    numbers: {
+      type: new GraphQLList(NumbersType),
+      description: "list of Numbers",
+      resolve: () => numbers,
+    },
+    number: {
+      type: NumbersType,
+      description: "One of Numbers",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) =>
+        numbers.find((number) => number.id === args.id),
+    },
+    string: {
+      type: StringsType,
+      description: "One of Strings",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) =>
+        strings.find((string) => string.id === args.id),
     },
   }),
 });
